e0079e687560f6e982cd4300f6d5826a1fc3adf5
David Meyer <david.meyer@appfolio.com>
2025-05-15 11:43:14 -0700

    Add new colorschemes to nvim

--- init.lua
+++ init.lua
@@ -877,13 +877,43 @@ require('lazy').setup({
     },
   },
 
+  -- Color schemes
+  { 'rose-pine/neovim', name = 'rose-pine' },
+
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_background = 'medium' -- 'soft', 'medium' (default), or 'hard'
+      vim.g.everforest_better_performance = 1
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
+
+  {
+    'sainnhe/sonokai',
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      -- Sonokai uses Vimscript-style settings
+      vim.g.sonokai_style = 'default' -- or 'andromeda', 'shusia', 'maia', 'espresso', 'atlantis'
+      vim.g.sonokai_enable_italic = 0 -- Disable italics
+
+      -- Load the colorscheme
+      -- vim.cmd.colorscheme 'sonokai'
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    -- priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
@@ -895,7 +925,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
