c1386c2655b223ad81ecdada62a311ee157e317e
DDM <theddmbassy@gmail.com>
2025-05-29 22:26:47 -0700

    Switch back to tokyonight because Annie likes it :)

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,9 +1,13 @@
 return {
   { -- lua/plugins/rose-pine.lua
     'rose-pine/neovim',
+    opts = {
+      dark_variant = 'main', -- main, moon, or dawn
+    },
     name = 'rose-pine',
-    config = function()
-      vim.cmd.colorscheme 'rose-pine'
+    config = function(_, opts)
+      require('rose-pine').setup(opts)
+      -- vim.cmd.colorscheme 'rose-pine'
     end,
   },
   {
@@ -32,7 +36,7 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
@@ -44,7 +48,7 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
   {
