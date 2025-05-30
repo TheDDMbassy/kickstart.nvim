0daa562cec9f4a1356893da0ccec4535c863a188
DDM <theddmbassy@gmail.com>
2025-05-30 03:17:09 -0700

    Get rid of warnings when loading Lua files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -249,6 +249,7 @@ return { -- LSP Plugins
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { globals = { 'vim', 'require' } },
             },
           },
         },
