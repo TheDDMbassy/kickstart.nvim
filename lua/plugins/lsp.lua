e5c28180376104c5325d95b29dff2e1067da00ad
DDM <theddmbassy@gmail.com>
2025-06-02 08:15:31 -0700

    Add vale as a markdown linter
    It's a bit noisy, but it gives good advice as well.
    ```
    nvim ~/Library/Application\ Support/vale/.vale.ini
    ```
    
    With these settings:
    
    StylesPath = styles
    
    MinAlertLevel = suggestion
    
    Packages = RedHat, alex
    
    [*.{md}]
    BasedOnStyles = Vale, RedHat, alex

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -271,6 +271,7 @@ return { -- LSP Plugins
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'vale',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
