e9ed0ecfb39fab049690ec75c4841bb41e6ba122
DDM <theddmbassy@gmail.com>
2025-06-29 12:58:55 +0100

    Trim trailing whitespace in elixir comments
    For some reason, the `mix` formatter allows comments to have trailing
    whitespace, which I don't prefer.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -30,7 +30,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      elixir = { 'mix' },
+      elixir = { 'mix', 'trim_whitespace' },
       eelixir = { 'mix' },
       heex = { 'mix' },
       surface = { 'mix' },
