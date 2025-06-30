280dedfa7909f704bee149e5af8fadf98d1479b3
DDM <theddmbassy@gmail.com>
2025-06-30 16:10:39 +0100

    Keymap for faster saves with <leader>w
    Typing :w<CR> is four keystrokes (LRLL on ergodox, and LRLR on laptop).
    
    This new keymap is a simple two-stroke RL command, with no major
    side-effects.

--- init.lua
+++ init.lua
@@ -148,6 +148,9 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Quicker saving
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
