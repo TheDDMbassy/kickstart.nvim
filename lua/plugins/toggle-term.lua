9abe15b4191eedd28f9cca613eb22525483403bd
DDM <theddmbassy@gmail.com>
2026-02-11 14:44:05 -0800

    New keyboard shortcut for running Elixir file

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -7,5 +7,12 @@ return {
     float_opts = {
       border = 'curved',
     },
+
+    -- Run current Elixir file in a new terminal window
+    vim.keymap.set('n', '<leader>re', function()
+      local file = vim.fn.expand '%:p' -- Get the full path of current file
+      -- vim.cmd('TermExec cmd="elixir ' .. vim.fn.shellescape(file) .. '"')
+      vim.cmd('TermExec cmd="elixir ' .. file .. '"')
+    end, { desc = 'Run Elixir file in ToggleTerm terminal' }),
   },
 }
